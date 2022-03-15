5989b40f1a3ba59cc12a8aa8a5bb96117e73bf1c
TrencsanszkyPatrik <90695000+TrencsanszkyPatrik@users.noreply.github.com>
2022-03-15 16:00:49 +0100

    Bejelentkezés/regisztráció

---
+++ js/bejelentkezes.js
@@ -0,0 +1,45 @@
+function reg(name, pass, pass2) {
+    if (localStorage.getItem('regisztralva') != "igen") {
+
+        if (pass == pass2) {
+            localStorage.setItem('username', name)
+            localStorage.setItem('password', pass)
+            localStorage.setItem('regisztralva', 'igen')
+        }
+        else if (pass != pass2) {
+            alert("Nem egyezik a két jelszó")
+        }
+    }
+    else {
+        alert("Már regisztráltál egyszer. Jelentkezz be!")
+    }
+}
+
+
+function log(name, pass){
+    if (localStorage.getItem('username') == name && localStorage.getItem('password') == pass){
+        alert("Sikeres bejelentkezés")
+        window.open('fooldal.html', "_self")
+    } else {
+        alert('Hiányzó adatok!')
+    }
+}
+
+
+
+
+function torles() {
+    localStorage.clear()
+}
+var xy = document.getElementById("log");
+var yx = document.getElementById("reg");
+
+function asd() {
+    xy.style.display = "block";
+    yx.style.display = "none";
+}
+
+function asd2() {
+    xy.style.display = "none";
+    yx.style.display = "block";
+}
